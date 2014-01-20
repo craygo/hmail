2ca41aab7859dbc0a4c6c1d41a01558e1d1852bc
Harry Binnendijk <harry@empanda.net>
2014-01-20 18:30:43 +1100

    intermediate commit

--- server/src/server/images.clj
+++ server/src/server/images.clj
@@ -1,11 +1,34 @@
 (ns server.images
   (:use [net.cgrand.enlive-html]
-  ))
+        [clojure.tools.logging])
+  (:import [java.io BufferedReader StringReader]))
+
+(defn split-on-curly [line]
+  (map #(str % "}") (clojure.string/split line #"}")))
+
+(defn safe-body [node]
+  (update-in node [:content]
+             (fn [s]
+               (let [s (first s)]
+                 (with-open [rdr1 (StringReader. s)
+                             rdr (BufferedReader. rdr1)]
+                   (let [lines (-> rdr line-seq)
+                         lines (if (> (count lines) 1)
+                                 lines
+                                 (split-on-curly (first lines)))]
+                     (->> lines
+                          (map #(if (re-find #"\{" %)
+                                  (str ".content " %)
+                                  %))
+                          (clojure.string/join "\n"))))))))
 
 (defn stop-images [content]
   (if (= (:type content) :html)
     (let [html-str (:content content)
           nodes (with-open [fl (java.io.StringReader. html-str)]
                   (html-resource fl))]
-      (assoc content :content (apply str (emit* (at nodes [:img] (remove-attr :src))))))
+      (assoc content :content (apply str (emit* (at nodes 
+                                                    [:img] (remove-attr :src)
+                                                    ;[:style] safe-body
+                                                    )))))
     content))
