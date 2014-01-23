155313d0b24c99d1875cd350b7d6c01a9c8cd40c
Harry Binnendijk <harry@empanda.net>
2014-01-24 09:51:56 +1100

    refactor and improve mail handling

--- server/src/server/messages.clj
+++ server/src/server/messages.clj
@@ -1,85 +1,52 @@
 (ns server.messages
   (:use [clojure.tools.logging])
-  (:require [server.mail :refer [get-store get-inbox prefetch-messages fetch-content as-message message-count]]
+  (:require [server.mail :as mail :refer [have-messages? get-content prefetch check-new]]
             [clojure.core.async :refer [go chan <! >! <!!]]
             [org.httpkit.server :refer [send!]]
             [server.images :refer [stop-images]]
             ))
 
-(def channels (atom {})) ; channel to {:store :messages}
-
-#_(-> @channels
-    first 
-    second
-    :store
-    (.getFolder "INBOX")
-    .isOpen
-    )
-
 (defmacro ws-send [channel & body]
   `(go (send! ~channel (pr-str (do ~@body)))))
 
 ;; message helpers
-(defn messages-mesg [channel & [init]]
-  {:type (if init :init :merge) :topic [:messages] :value (get-in @channels [channel :client-messages-by-id])})
+(defn messages-mesg [channel msgs & [init]]
+  (let [mesg {:type (if init :init :merge) :topic [:messages] :value msgs}]
+    ;(info "messages-mesg " mesg)
+    mesg))
 
-(defn fetch-content-for-all-messages [channel]
-  ;(info "fetch-content-for-all-messages " channel)
-  (let [msgs (get-in @channels [channel :raw-messages])]
-    (doseq [mesg msgs]
-      (go (let [[id content] (fetch-content mesg)
+(defn fetch-content-for-messages [channel msgs]
+  (let [msg-nums (keys msgs)]
+    (doseq [msg-num msg-nums]
+      (go (let [content (get-content channel "INBOX" msg-num)
                 content (stop-images content)]
-            ;(info "fetch-content-for-all-messages " id (count content))
-            (swap! channels assoc-in [channel :client-messages-by-id id :content] content)
-            (send! channel (pr-str (messages-mesg channel))))))))
+            (send! channel (pr-str (messages-mesg channel {msg-num {:content content}}))))))))
 
 (defn prefetch-top-messages [channel n]
-  (let [inbox (get-in @channels [channel :inbox])]
-    (info "prefetch-top-messages inbox " inbox)
-    (let [msgs (prefetch-messages inbox n)
-          client-msgs (map as-message msgs)
-          client-msgs (into {} (map #(vector (:id %) %) client-msgs))]
-      (swap! channels update-in [channel :raw-messages] concat msgs)
-      (swap! channels update-in [channel :client-messages-by-id] merge client-msgs)
-      (fetch-content-for-all-messages channel))))
-
-(defn have-messages? [channel]
-  (seq (get-in @channels [channel :client-messages-by-id])))
-
-(defn check-new [channel inbox]
-  (let [msg-cnt (message-count inbox)
-        highest (apply max (keys (get-in @channels [channel :client-messages-by-id])))]
-    (if (> msg-cnt highest)
-      (prefetch-top-messages channel (- msg-cnt highest)))
-    (messages-mesg channel)))
+  (let [msgs (prefetch channel "INBOX" n 2)]
+    (fetch-content-for-messages channel msgs)
+    msgs))
 
 (def prefetch-size 3)
 
 ;; handlers
 (defn init-messages [channel]
-  (if-not (have-messages? channel)
+  (if-not (have-messages? channel "INBOX")
     (do
       (ws-send channel 
-               (prefetch-top-messages channel prefetch-size)
-               (messages-mesg channel :init))
+               (let [msgs (prefetch-top-messages channel prefetch-size)]
+                 (messages-mesg channel msgs :init)))
       [])
-    (check-new channel (get-in @channels [channel :inbox]))))
+    (let [msgs (check-new channel "INBOX")]
+      (fetch-content-for-messages channel msgs)
+      (messages-mesg channel msgs))))
 
 (defn login [mesg channel]
   (try 
     (let [value (:value mesg)
-          {:keys [username password server]} value
-          store* (get-store username password server)]
-      (info "login " username server)
-      (if (.isConnected store*)
-        (let [inbox (get-inbox store*)]
-          (info "login connected inbox " inbox)
-          (swap! channels assoc-in [channel :store] store*)
-          (swap! channels assoc-in [channel :inbox] inbox)
-          {:type :update :topic [:user] :value {:name username}})
-        (do
-          (info "login NOT connected")
-          {:type :update :topic [:user] :value {:msg "failed"}})))
+          {:keys [username password server]} value]
+      (mail/login channel username password server)
+      {:type :update :topic [:user] :value {:name username}})
     (catch javax.mail.AuthenticationFailedException e
       (info "login " e)
       {:type :update :topic [:user] :value {:msg (.getMessage e)}})
@@ -88,21 +55,21 @@
       {:type :update :topic [:user] :value {:msg (.getMessage e)}})))
 
 (defn logout [channel]
-  (swap! channels assoc-in [channel] nil)
+  (mail/logout channel)
   {:type :update :topic [:user] :value {}})
 
 (defn close [channel]
-  (swap! channels dissoc channel))
+  (mail/close channel))
 
 (defn handle-message [{:keys [type topic] :as mesg} channel]
   (try
     ;(info "handle-message " mesg)
     (condp = type
-      :init (init-messages channel)
-      :login (login mesg channel)
-      :logout (logout channel)
-      :close (close channel)
-      (info "no handler for: " (:dissoc mesg :value))
-      )
+                :init (init-messages channel)
+                :login (login mesg channel)
+                :logout (logout channel)
+                :close (close channel)
+                (info "no handler for: " (:dissoc mesg :value))
+                )
     (catch Exception e
       (error e e))))
